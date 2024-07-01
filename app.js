44ec1de6ba4c540bdd72757eaabc5ec4ef512083
aescobar <aescobar@sbp.cl>
2024-07-01 00:59:12 -0400

    fix: update port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const routes = require('./routes/index');
 
 
